a0ea23e8feac59582052c8636ff9d80faf268f78
Ivan-Mol <abyss.devotee@gmail.com>
2023-10-05 11:14:42 +0200

    Permissions added

--- src/main/java/com/ivanmol/javaspringsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/ivanmol/javaspringsecuritydemo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ivanmol.javaspringsecuritydemo.config;
 
+import com.ivanmol.javaspringsecuritydemo.model.Permission;
 import com.ivanmol.javaspringsecuritydemo.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,9 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole(Role.ADMIN.name(),Role.USER.name())
-                .antMatchers(HttpMethod.POST,"/api/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE,"/api/**").hasAnyRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET,"/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+                .antMatchers(HttpMethod.POST,"/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE,"/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -40,13 +41,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .builder()
                         .username("admin")
                         .password(passwordEncoder().encode("admin"))
-                        .roles(Role.ADMIN.name())
+                        .authorities(Role.ADMIN.getAuthorities())
                         .build(),
                 User
                         .builder()
                         .username("user")
                         .password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name())
+                        .authorities(Role.USER.getAuthorities())
                         .build()
         );
     }
---
+++ src/main/java/com/ivanmol/javaspringsecuritydemo/model/Permission.java
@@ -0,0 +1,14 @@
+package com.ivanmol.javaspringsecuritydemo.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Permission {
+    DEVELOPERS_READ("developers:read"),
+    DEVELOPERS_WRITE("developers:write");
+
+    private final String permission;
+    Permission(String permission) {
+        this.permission = permission;
+    }
+}
